b51245d95a84e2c1ebc7afbd7c4d5736499294f0
Nick <studyaga@mail.ru>
2021-06-02 11:19:15 +0300

    -some fixes[3]

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <nav className='grey darken-4 header nav-wraper'>
             <div className="nav-wrapper">
-                <a href="/" className="brand-logo">Image Finder</a>
+                <a href="/flickr-search" className="brand-logo">Image Finder</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
                         <a href="https://github.com/the-lensky/flickr-search" rel="noreferrer" target="_blank">
